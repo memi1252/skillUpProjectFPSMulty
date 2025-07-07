6311f24d31adffe0a00ef8bb5864acb08aa7d3e9
lmae8465 <165880493+lmae8465@users.noreply.github.com>
2025-07-07 16:30:53 +0900

    1

--- Assets/MFPS/Scripts/Internal/General/bl_GameTexts.cs
+++ Assets/MFPS/Scripts/Internal/General/bl_GameTexts.cs
@@ -1,103 +1,103 @@
-﻿public static class bl_GameTexts 
+﻿public static class bl_GameTexts
 {
-    public static string Players = "Players";
+    public static string Players = "플레이어들";
 
-    public static string TDM = "Team Death Match";
-    public static string FFA = "Free For All";
-    public static string CTF = "Capture The Flag";
-    public static string SND = "Search And Destroy";
-    public static string CP = "Cover Point";
-    public static string GR = "Gun Race";
-    public static string LSM = "Last Man Standing";
-    public static string ELIM = "Elimination";
-    public static string KC = "Kill Confirmed";
+    public static string TDM = "팀 데스매치";
+    public static string FFA = "자유 대 자유";
+    public static string CTF = "깃발 잡기";
+    public static string SND = "수색 및 파괴";
+    public static string CP = "커버 포인트";
+    public static string GR = "건 레이스";
+    public static string LSM = "마지막 생존자";
+    public static string ELIM = "제거";
+    public static string KC = "킬 확인";
 
-    public static string PickUpMedicalKit = "Pick up medical kit";
-    public static string PickUpAmmoKit = "Pick up Ammo";
-    public static string PickUpWeapon = "Press {1} for pick up <color=#70ABFF>{0}</color>";
-    public static string PickUpWeaponEquipped = "<color=#70ABFF>{0}</color> is already equipped.";
-    public static string Touch = "Touch";
-    public static string OutOfAmmo = "Out of ammo";
-    public static string Reload = "Reload";
-    public static string LowAmmo = "Low Ammo";
-    public const string OpenDoor = "Open door";
-    public const string CloseDoor = "Close door";
-    public const string JumpOffLadder = "Jump off of ladder";
-    public const string TryAgain = "Try again";
-    public const string Ok = "Ok";
+    public static string PickUpMedicalKit = "의료 키트를 줍다";
+    public static string PickUpAmmoKit = "탄약 키트를 줍다";
+    public static string PickUpWeapon = "무기 <color=#70ABFF>{0}</color>를(을) 줍기 위해 {1} 키를 누르세요";
+    public static string PickUpWeaponEquipped = "<color=#70ABFF>{0}</color>는 이미 장착되어 있습니다.";
+    public static string Touch = "터치";
+    public static string OutOfAmmo = "탄약이 부족합니다";
+    public static string Reload = "재장전";
+    public static string LowAmmo = "탄약 부족";
+    public const string OpenDoor = "문 열기";
+    public const string CloseDoor = "문 닫기";
+    public const string JumpOffLadder = "사다리에서 뛰어내리기";
+    public const string TryAgain = "다시 시도";
+    public const string Ok = "확인";
 
-    public static string PlayerStart = "PLAYER STAR:\n<size=8><color=black>{0}</color></size>";
-    public static string Killed = "Killed";
-    public static string HeadShot = "HeadShot!";
-    public static string JoinedInMatch = "Joined in the match";
-    public static string LeftOfMatch = "Left of match";
-    public static string CommittedSuicide = "Has committed suicide";
-    public static string DeathByFall = "Death by Fall";
-    public static string KilledEnemy = "Killed Enemy";
-    public static string HeatShotBonus = "HeadShot Bonus";
-    public static string JoinIn = "Join in ";
-    public static string GRPlayerWinning = "PLAYER WINNING:\n<size=10><color=black>{0} | {1}/{2}</color></size>";
-    public static string NoLimit = "No Limit";
-    public static string Spectating = "Spectating";
-    public static string KillingInAction = "KILLED IN ACTION";
-    public static string KillZoneMessage = "Return to the battlefield in\n<color=red><size=25>{0}</size>s</color>";
+    public static string PlayerStart = "플레이어 스타트:\n<size=8><color=black>{0}</color></size>";
+    public static string Killed = "처치됨";
+    public static string HeadShot = "헤드샷!";
+    public static string JoinedInMatch = "경기에 참여함";
+    public static string LeftOfMatch = "경기에서 나감";
+    public static string CommittedSuicide = "자살함";
+    public static string DeathByFall = "낙사";
+    public static string KilledEnemy = "적 처치";
+    public static string HeatShotBonus = "헤드샷 보너스";
+    public static string JoinIn = "참여 중 ";
+    public static string GRPlayerWinning = "플레이어 승리:\n<size=10><color=black>{0} | {1}/{2}</color></size>";
+    public static string NoLimit = "제한 없음";
+    public static string Spectating = "관전 중";
+    public static string KillingInAction = "전사";
+    public static string KillZoneMessage = "전장으로 복귀\n<color=red><size=25>{0}</size>초</color>";
 
-    public static string ServerMesagge = "[Server]";
-    public static string OpenChatStart = "[Server]: Press 'T' or 'Y' for open chat box";
-    public static string NoOneWonName = "No One";
-    public static string AFKWarning = "You are AFK, you will be kicked in: {0}";
+    public static string ServerMesagge = "[서버]";
+    public static string OpenChatStart = "[서버]: 채팅 창을 열려면 'T' 또는 'Y'를 누르세요";
+    public static string NoOneWonName = "승리한 사람 없음";
+    public static string AFKWarning = "AFK 상태입니다. {0}초 후에 강제 퇴장됩니다.";
 
-    public static string FinalOneMatch = "Return to Lobby in";
-    public static string FinalRounds = "Next Round in";
-    public static string FinalWinner = "Won this round!";
-    public static string NoRoomsCreated = "THERE ARE NOT ROOMS CREATED YET, CREATE ONE TO PLAY.";
-    public static string Spectators = "<b>{0}</b> SPECTATORS";
-    public static string WaitingPlayers = "WAITING FOR PLAYERS <b>{0} OF {1}</b>";
-    public static string RespawnIn = "RESPAWN IN: {0}";
-    public static string LoadingLocalContent = "LOADING LOCAL GAME CONTENT...";
-    public static string ConnectingToGameServer = "CONNECTING TO THE SERVER...";
-    public static string DisconnectCause = "<size=25><b>OPPS!</b></size>\nCOULD NOT CONNECT TO THE SERVER.\nCAUSE: {0}";
-    public static string StartingOfflineRoom = "STARTING OFFLINE ROOM...";
-    public static string StartingMatch = "STARTING MATCH...";
-    public static string QuitGameConfirmation = "<b>Quit game?</b>\n\nAre you sure you want to quit the game?";
-    public static string WaitingTeamBalance = "Waiting for balance team players";
-    public const string AnotherSessionOpen = "Your account has connected from another game session.";
-    public const string FinishingUp = "FINISHING UP...";
-    public const string VerifyingSession = "VERIFYING PLAYER SESSION...";
-    public const string ChangeRegionAsk = "Change server region?";
-    public const string LeaveMatchWarning = "ARE YOU SURE TO LEAVE THE MATCH?";
+    public static string FinalOneMatch = "로비로 돌아가기 {0}";
+    public static string FinalRounds = "다음 라운드 {0}초 후";
+    public static string FinalWinner = "이번 라운드 승리!";
+    public static string NoRoomsCreated = "현재 생성된 방이 없습니다. 방을 생성하여 플레이하세요.";
+    public static string Spectators = "<b>{0}</b> 명 관전 중";
+    public static string WaitingPlayers = "플레이어 대기 중 <b>{0} / {1}</b>";
+    public static string RespawnIn = "재생성 대기: {0}초";
+    public static string LoadingLocalContent = "게임 콘텐츠 로딩중...";
+    public static string ConnectingToGameServer = "서버에 접속중...";
+    public static string DisconnectCause = "<size=25><b>앗!</b></size>\n서버에 연결할 수 없습니다.\n원인: {0}";
+    public static string StartingOfflineRoom = "오프라인 방 시작 중...";
+    public static string StartingMatch = "경기 시작 중...";
+    public static string QuitGameConfirmation = "<b>게임을 종료하시겠습니까?</b>\n\n정말로 게임을 종료하시겠습니까?";
+    public static string WaitingTeamBalance = "팀 균형 맞추는 중";
+    public const string AnotherSessionOpen = "귀하의 계정이 다른 게임 세션에 연결되었습니다.";
+    public const string FinishingUp = "거의 완료됨...";
+    public const string VerifyingSession = "플레이어 세션 확인중...";
+    public const string ChangeRegionAsk = "서버 지역을 변경하시겠습니까?";
+    public const string LeaveMatchWarning = "경기에서 나가시겠습니까?";
 
-    public static string CaptureTheFlag = "Has capture Flag";
-    public static string ObtainedFlag = "Obtained at the {0} flag";
-    public static string CoverPointTime = "TAKING POINT IN: <b>{0}</b>";
+    public static string CaptureTheFlag = "깃발을 잡았습니다";
+    public static string ObtainedFlag = "깃발 {0}을(를) 획득";
+    public static string CoverPointTime = "포인트 확보 중: <b>{0}</b>";
 
-    public static string FireTypeAuto = "AUTO";
-    public static string FireTypeSemi = "SEMI";
-    public static string FireTypeSingle = "SINGLE";
+    public static string FireTypeAuto = "자동";
+    public static string FireTypeSemi = "반자동";
+    public static string FireTypeSingle = "단발";
 
-    public static string[] AntiStropicOptions = new string[] { "Disable", "Enable", "Force Enable" };
+    public static string[] AntiStropicOptions = new string[] { "비활성화", "활성화", "강제로 활성화" };
 
-    public static string RoomsCreated = "<b>ROOMS CREATED:</b> {0}";
-    public static string PlayersOnline = "<b>PLAYERS ONLINE:</b> {0}";
-    public static string PlayersPlaying = "<b>PLAYERS PLAYING:</b> {0}";
-    public static string PlayersInLobby = "<b>PLAYERS IN LOBBY:</b> {0}";
+    public static string RoomsCreated = "<b>생성된 방:</b> {0}";
+    public static string PlayersOnline = "<b>온라인 플레이어:</b> {0}";
+    public static string PlayersPlaying = "<b>플레이 중인 플레이어:</b> {0}";
+    public static string PlayersInLobby = "<b>로비에 있는 플레이어:</b> {0}";
 
-    public static string KickConfirmation = "{0} HAS BEEN KICKED!";
-    public static string KickFailed = "{0} HAS NOT BEEN KICKED!";
-    public static string VoteBy = "VOTE BY: {0}";
-    public static string KickQuestion = "KICK '{0}'?";
-    public static string YouVote = "YOU VOTE {0}";
+    public static string KickConfirmation = "{0} 님이 퇴장되었습니다!";
+    public static string KickFailed = "{0} 님을 퇴장시키지 못했습니다!";
+    public static string VoteBy = "투표자: {0}";
+    public static string KickQuestion = "'{0}' 님을 퇴장시키시겠습니까?";
+    public static string YouVote = "당신의 투표: {0}";
 
-    public const string LeaveRoomConfirmation = "Are you sure to leave this room?";
-    public const string EnterAsSpectator = "Are you sure to enter as spectator?";
+    public const string LeaveRoomConfirmation = "이 방을 떠나시겠습니까?";
+    public const string EnterAsSpectator = "관전자로 입장하시겠습니까?";
 
-    //Game Resume
-    public static string Kills = "Kills";
-    public static string Deaths = "Deaths";
-    public static string Score = "Score";
-    public static string TimePlayed = "Time Played";
-    public static string WinMatch = "Win Match: +{0}";
-    public static string TotalScore = "Total Score";
+    // 게임 통계
+    public static string Kills = "킬";
+    public static string Deaths = "사망";
+    public static string Score = "점수";
+    public static string TimePlayed = "플레이 시간";
+    public static string WinMatch = "경기 승리: +{0}";
+    public static string TotalScore = "총 점수";
 
     public static readonly string[] RandomNames = new string[] { "John", "William", "Michael", "Jason", "Aaron", "Angie", "Benjamin", "Cody", "Dany", "Gao",
         "Jacob", "Jeese","Katherin","Lee","Massimo", "Oliver", "Pride", "Sarah", "Thomas", "Tuan", "Johnson", "Lovo", "Patick", "Derek", "Felix", "Harry",
